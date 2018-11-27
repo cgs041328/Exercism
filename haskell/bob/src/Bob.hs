58435074dd5171d8389ffed984858c9ef6f22c1c
Ganshen Chen <cgs931219@outlook.com>
2018-11-27 17:40:22 +0800

    bob

--- haskell/bob/src/Bob.hs
+++ haskell/bob/src/Bob.hs
@@ -1,4 +1,17 @@
 module Bob (responseFor) where
 
+import Data.Char (isAlpha, isSpace, isUpper)
+import Data.List (isPrefixOf, dropWhile)
+import Control.Arrow ((&&&))
+
 responseFor :: String -> String
-responseFor xs = error "You need to implement this function."
+responseFor xs
+  | isSilence xs                  = "Fine. Be that way!"
+  | isYelling xs && isQuestion xs = "Calm down, I know what I'm doing!"
+  | isYelling xs                  = "Whoa, chill out!"
+  | isQuestion xs                 = "Sure."
+  | otherwise                     = "Whatever."
+    where  isSilence = all isSpace
+           isYelling ys = ((not.null &&& all isUpper) $ filter isAlpha ys) == (True, True)
+           isQuestion = isPrefixOf "?". dropWhile isSpace. reverse
+
