d673909b920db84e576dcfc43a636d6bb6e0b1bf
Ganshen Chen <cgs931219@outlook.com>
2018-11-27 14:33:21 +0800

    leap

---
+++ haskell/leap/src/LeapYear.hs
@@ -0,0 +1,10 @@
+module LeapYear (isLeapYear) where
+
+isLeapYear :: Integer -> Bool
+isLeapYear year
+    | year `isDivisibleBy` 400 = True
+    | year `isDivisibleBy` 100 = False
+    | year `isDivisibleBy` 4   = True
+    | otherwise                = False
+  where isDivisibleBy a b = a `rem` b == 0
+
