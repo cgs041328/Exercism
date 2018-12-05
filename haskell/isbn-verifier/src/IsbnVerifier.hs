df6b33db0e60e95232f45ffd6930b1f0c37b55e5
Ganshen Chen <cgs931219@outlook.com>
2018-12-05 12:01:47 +0800

    isbn

--- haskell/isbn-verifier/src/IsbnVerifier.hs
+++ haskell/isbn-verifier/src/IsbnVerifier.hs
@@ -1,4 +1,11 @@
 module IsbnVerifier (isbn) where
 
+import Data.Char(isNumber, digitToInt)
+
 isbn :: String -> Bool
-isbn = error "You need to implement this function."
+isbn s = if (length s' == 10 && all isNumber (take 9 s'))
+         then sum' `mod` 11 == 0
+         else False
+    where s' = filter (\x -> isNumber x || x == 'X') s
+          sum' =sum. zipWith (*) (reverse [1..10]) $ map (\x -> if x == 'X' then 10 else digitToInt x) s'
+
