79fde7e3945d0ba5ac560c57d713e856b6ecc950
Ganshen Chen <cgs931219@outlook.com>
2018-12-19 10:14:15 +0800

    optimize collatz conjecture

--- haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -3,11 +3,10 @@ module CollatzConjecture (collatz) where
 
 collatz :: Integer -> Maybe Integer
 collatz n
-    | n >= 1    = Just $ fromIntegral $ length $ drop 1 $ takeWhile (>=1) $ iterate step n
+    | n >= 1    = Just $ fromIntegral $ length $ takeWhile (>1) $ iterate step n
     | otherwise = Nothing
 
 step :: Integer -> Integer
 step n
-  | n == 1    = 0
-  | even n    = n `div` 2
+  | even n    = n `quot` 2
   | otherwise = n * 3 + 1
