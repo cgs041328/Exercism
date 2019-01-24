a68202b576f33751bdb6e088d3d0be350ca61db8
Ganshen Chen <cgs931219@outlook.com>
2019-01-24 16:26:30 +0800

    series

---
+++ haskell/series/src/Series.hs
@@ -0,0 +1,6 @@
+module Series (slices) where
+
+import Data.Char(digitToInt)    
+
+slices :: Int -> String -> [[Int]]
+slices n xs = fmap (\i -> (fmap digitToInt. take n. drop i) xs) [0..length xs - n]
