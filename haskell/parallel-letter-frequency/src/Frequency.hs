ef26f45647c0d7e0fcaa7a84897d2fd51aaa14a9
Ganshen Chen <cgs931219@outlook.com>
2019-02-15 11:58:45 +0800

    frequency

---
+++ haskell/parallel-letter-frequency/src/Frequency.hs
@@ -0,0 +1,15 @@
+module Frequency (frequency) where
+
+import qualified Control.Parallel.Strategies as S
+import Data.Char ( isLetter, toLower )
+import qualified Data.Map.Strict as M
+import qualified Data.Text as T
+
+frequency :: Int -> [T.Text] -> M.Map Char Int
+frequency n = M.unionsWith (+)
+            . S.withStrategy (S.parBuffer n S.rdeepseq)
+            . map frequency'
+
+frequency' :: T.Text -> M.Map Char Int
+frequency' = T.foldl' f M.empty . T.filter isLetter
+  where f counts char = M.insertWith (+) (toLower char) 1 counts
