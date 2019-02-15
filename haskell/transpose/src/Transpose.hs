81b4cd47cd25dc78c3ce45ea42a766d85fa236da
Ganshen Chen <cgs931219@outlook.com>
2019-02-15 17:00:46 +0800

    transpose

---
+++ haskell/transpose/src/Transpose.hs
@@ -0,0 +1,12 @@
+module Transpose (transpose) where
+
+import qualified Data.List (transpose)
+
+transpose :: [String] -> [String]
+transpose = Data.List.transpose . padStrings
+  where
+    padStrings = zipWith padRight . scanr1 max. fmap length
+    padRight n s = s ++ replicate (n - length s) ' '
+
+
+           
